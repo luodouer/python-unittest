939dfeca8690be49bda4d2bb14ab3085f6b67cae
luodouer <1376324108@qq.com>
2019-05-05 21:55:27 +0800

    updata run.py

--- douzi_0419/run.py
+++ douzi_0419/run.py
@@ -1,16 +1,20 @@
 # encoding=utf_8
 # @Author  ： 豆子
 
+import sys
 import time
 import unittest
 import os
 from HTMLTestRunner import HTMLTestRunner
-
 from douzi_0419.commons import filepath
+
+# a = sys.path
+# print(a)
+sys.path.append('./')
 
 discover = unittest.defaultTestLoader.discover(filepath.case_dir, 'test*.py')
 now = time.strftime('%Y%m%d_%H%M%S', time.localtime(time.time()))
-report_name = 'report' + '_' + noddw + '.htmlddddddd'
+report_name = 'report' + '_' + now + '.html'
 report_path = os.path.join(filepath.report_dir, report_name)
 with open(report_path, 'wb+') as file:
     runner = HTMLTestRunner(stream=file, title='测试报告', description='api测试')
